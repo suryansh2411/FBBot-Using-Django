95bffdcb29aa441f3d41a07e49f6c5ccc425b5b0
suryansh2411 <sharma.hs67@gmail.com>
2020-08-23 22:45:57 +0530

    First Commit

---
+++ bot/views.py
@@ -0,0 +1,56 @@
+import json, requests, random, re
+from pprint import pprint
+
+from django.views import generic
+from django.http.response import HttpResponse
+
+from django.views.decorators.csrf import csrf_exempt
+from django.utils.decorators import method_decorator
+
+PAGE_ACCESS_TOKEN = ""
+VERIFY_TOKEN = "2318934571"
+
+text = { 'hi': ["""Hey There !!"""], 
+         'who': ["""I'm a bot which will help in future. """], 
+         'bye': ["""Bye. See you soon."""] }
+
+def post_facebook_message(fbid, recevied_message):
+    # Remove all punctuations, lower case the text and split it based on space
+    tokens = re.sub(r"[^a-zA-Z0-9\s]",' ',recevied_message).lower().split()
+    msg_text = ''
+    for token in tokens:
+        if token in text:
+            msg_text = random.choice(text[token])
+            break
+    if not msg_text:
+        msg_text = "I didn't understand! Send 'hi', 'who are you ?', 'bye' for a reply" 
+                   
+    post_message_url = 'https://graph.facebook.com/v2.6/me/messages?access_token=%s'%PAGE_ACCESS_TOKEN
+    response_msg = json.dumps({"recipient":{"id":fbid}, "message":{"text":msg_text}})
+    status = requests.post(post_message_url, headers={"Content-Type": "application/json"},data=response_msg)
+    pprint(status.json())
+
+# Create your views here.
+class BotView(generic.View):
+    def get(self, request, *args, **kwargs):
+        if self.request.GET['hub.verify_token'] == VERIFY_TOKEN:
+            return HttpResponse(self.request.GET['hub.challenge'])
+        else:
+            return HttpResponse('Error, invalid token')
+        
+    @method_decorator(csrf_exempt)
+    def dispatch(self, request, *args, **kwargs):
+        return generic.View.dispatch(self, request, *args, **kwargs)
+
+    # Post function to handle Facebook messages
+    def post(self, request, *args, **kwargs):
+        incoming_message = json.loads(self.request.body.decode('utf-8'))
+        for entry in incoming_message['entry']:
+            for message in entry['messaging']:
+                if 'message' in message:
+                    # Print the message to the terminal
+                    pprint(message)    
+                    # Assuming the sender only sends text. Non-text messages like stickers, audio, pictures
+                    # are sent as attachments and must be handled accordingly. 
+                    post_facebook_message(message['sender']['id'], message['message']['text'])    
+        return HttpResponse()
